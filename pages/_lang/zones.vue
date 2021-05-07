a23c1bc37adfe0416575a1398e08ba6c7b65bb7a
Jattapol <jattapol.aom@gmail.com>
2021-05-07 13:06:56 +0700

    update get zones

--- pages/_lang/zones.vue
+++ pages/_lang/zones.vue
@@ -29,7 +29,6 @@
 import { defineComponent } from '@nuxtjs/composition-api'
 import Dropdown from '@/components/forms/Dropdown.vue'
 import Pagination from '@/components/menus/Pagination.vue'
-import { getProvinces } from '@/helpers/getData'
 import { ZonesService } from '@/services'
 import { IinsertZone } from '@/types/zone'
 
@@ -41,15 +40,6 @@ export default defineComponent({
 
   data() {
     return {
-      seletedProvince: null,
-      dropdownProvince: [
-        ...getProvinces().map((e) => ({
-          content: e.PROVINCE_NAME,
-          value: e.PROVINCE_ID,
-        })),
-      ],
-      dropdownDistrict: [{ content: '', value: 0 }],
-      seletedDistrict: null,
       perPage: 30,
       value: {
         page: 1,
@@ -63,17 +53,6 @@ export default defineComponent({
         },
       },
     }
-  },
-  watch: {
-    seletedProvince(to) {
-      // this.dropdownDistrict = getDistricts(to).map((e) => ({
-      //   content: e.DISTRICT_NAME,
-      //   value: e.DISTRICT_ID,
-      // }))
-    },
-    seletedDistrict(to) {
-      // (this.zones as any) = getZones(to)
-    },
   },
   mounted() {
     console.log(this.value.totalPages)
